65f749169efbdd96561381a788306bd4899becdc
ABDELRHMAN <abdelrhmanaiid@wowws.net>
2025-06-23 11:25:16 +0300

    Videos 9 and 10

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 
 
 // video 3 - 8
+/*
 var data = 5 , dAta = 6;
 var result = data + dAta;
 console.log( data , dAta , result );
@@ -19,4 +20,16 @@ console.log("**************************************************");
 x += data;
 console.log(x);
 console.log("**************************************************");
+*/
 
+// video 9 - 10
+console.log("xav"<"333");
+console.log("xav"<"xavs");
+console.log("**************************************************");
+var x = 2,z=3;
+function test() {
+    return true;
+}
+console.log( x==z && test());
+console.log( x==z || test());
+console.log("**************************************************");
