fcfb80a723ecc43f34434c2a13eb93f51bbe445e
ABDELRHMAN <abdelrhmanaiid@wowws.net>
2025-06-23 10:20:38 +0300

    Videos from 3 to 8

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 // Hello nodejs
+/**************************************************      */
+
+
+// video 3 - 8
+var data = 5 , dAta = 6;
+var result = data + dAta;
+console.log( data , dAta , result );
+console.log("**************************************************");
+
+var x = 10;
+var result2 = ++x;
+console.log(result2);
+console.log("**************************************************");
+
+console.log((5*2) + 6**3 * 2);
+console.log("**************************************************");
+
+x += data;
+console.log(x);
+console.log("**************************************************");
+
